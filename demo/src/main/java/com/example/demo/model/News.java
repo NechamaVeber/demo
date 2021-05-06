2147e2c423ce04c402ce752b6f56e13ad122c4a6
NechamaVeber <v0548500151@gmail.com>
2021-05-06 10:52:04 +0300

    fix

--- demo/src/main/java/com/example/demo/model/News.java
+++ demo/src/main/java/com/example/demo/model/News.java
@@ -95,6 +95,16 @@ public News(source source, String author, String title, String description, Stri
 	this.publishedAt = publishedAt;
 	this.content = content;
 }
+public News(com.example.demo.model.source source, String title, String description, String url, String urlToImage,
+		String content) {
+	super();
+	this.source = source;
+	this.title = title;
+	this.description = description;
+	this.url = url;
+	this.urlToImage = urlToImage;
+	this.content = content;
+}
 
 
 }
